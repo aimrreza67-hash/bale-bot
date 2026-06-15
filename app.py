b7aff2969b09a40549d13da82c8979b67757c166
aimrreza67-hash <aimrreza67@gmail.com>
2026-06-15 13:14:31 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,20 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+db = {}
+
 @app.route("/")
 def home():
     return "Bot is running"
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    data = request.get_json()
-    print(data)
-    return "ok"
+
 def add_member(db, group_id, username):
     if group_id not in db:
         db[group_id] = {"members": []}
 
     if username not in db[group_id]["members"]:
         db[group_id]["members"].append(username)
-db = {}
+
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -28,10 +26,11 @@ def webhook():
     username = message.get("from", {}).get("username")
     group_id = message.get("chat", {}).get("id")
 
+     
     if username:
         add_member(db, group_id, f"@{username}")
 
-
+    
     if text == "@all":
         members = db.get(group_id, {}).get("members", [])
         return " ".join(members)
