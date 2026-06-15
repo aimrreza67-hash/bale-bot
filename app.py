fb2ed78822a314d0e134e0757e93f4e5024b7f5a
aimrreza67-hash <aimrreza67@gmail.com>
2026-06-15 13:07:13 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Bot is running"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+    print(data)
+    return "ok"
