b0139ad36b3f980f923d5d9ae44927abd07318a4
aimrreza67-hash <aimrreza67@gmail.com>
2026-06-15 13:10:45 +0330

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,29 @@ def webhook():
     data = request.get_json()
     print(data)
     return "ok"
+def add_member(db, group_id, username):
+    if group_id not in db:
+        db[group_id] = {"members": []}
+
+    if username not in db[group_id]["members"]:
+        db[group_id]["members"].append(username)
+db = {}
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+
+    message = data.get("message", {})
+    text = message.get("text", "")
+    username = message.get("from", {}).get("username")
+    group_id = message.get("chat", {}).get("id")
+
+    if username:
+        add_member(db, group_id, f"@{username}")
+
+
+    if text == "@all":
+        members = db.get(group_id, {}).get("members", [])
+        return " ".join(members)
+
+    return "ok"
